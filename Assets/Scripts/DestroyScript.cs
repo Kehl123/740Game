d6984a480adae3fe4e9353e311dca5bc628c85a1
Kehl123 <107139591+Kehl123@users.noreply.github.com>
2022-12-06 16:15:05 +0000

    things

--- Assets/Scripts/DestroyScript.cs
+++ Assets/Scripts/DestroyScript.cs
@@ -8,16 +8,18 @@ public class DestroyScript : MonoBehaviour
     bool destroyed;
     public Sprite destroyedSprite;
     public SpriteRenderer spriteRenderer;
+    //Animation anim
 
     public void destroyedEvent()
     {
         spriteRenderer.sprite = destroyedSprite;
+        //anim.Play();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //anim = GteComponent<Animation>();
     }
 
     // Update is called once per frame
